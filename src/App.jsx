e686cd00f2ea46673c4bd472baf3fb66e7631f90
seonghoon <shee5032@naver.com>
2025-02-15 18:11:50 +0900

    feat: 테스트용 RGB 데이터 조회 API 개발

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 import "./App.css";
 import SearchResultBox from "./components/bottomContent/searchResultBox";
 import SearchInputBox from "./components/topContent/searchInputBox";
+import { SERVER_URL } from "./constants/constants";
 
 function App() {
+  const [rgbData, setRgbData] = useState([]);
+
+  const fetchRgbData = async (url) => {
+    const encodedUrl = encodeURIComponent(url);
+    const response = await fetch(`${SERVER_URL}/crawl/${encodedUrl}`);
+    const data = await response.json();
+    setRgbData(data);
+  };
+
+  // console.log(rgbData);
   return (
     <>
       <h2>Color Extractor</h2>
-      <SearchInputBox />
-      <SearchResultBox />
+      <SearchInputBox onSearch={fetchRgbData} />
+      <SearchResultBox rgbData={rgbData} />
     </>
   );
 }
--- src/components/topContent/searchInputBox.jsx
+++ src/components/topContent/searchInputBox.jsx
@@ -1,11 +1,32 @@
-function SearchInputBox() {
+import { useState } from "react";
+
+function SearchInputBox({ onSearch }) {
+  const [url, setUrl] = useState("");
+
+  const handleSearch = async () => {
+    await onSearch(url);
+  };
+
+  const handleInputUrl = (event) => {
+    setUrl(event.currentTarget.value);
+  };
+
   return (
     <div className="flex items-center gap-2 bg-gray-300">
       <input
         type="text"
         className="flex-1 bg-gray-300 text-black outline-none"
+        value={url}
+        onChange={(event) => {
+          handleInputUrl(event);
+        }}
       />
-      <button className="bg-black p-2 text-white">search</button>
+      <button
+        className="bg-black p-2 text-white"
+        onClick={handleSearch}
+      >
+        search
+      </button>
     </div>
   );
 }
