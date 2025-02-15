bfad45c23419e7fef63232ce1ac22e46b5a8c304
seonghoon <shee5032@naver.com>
2025-02-15 18:11:50 +0900

    feat: 테스트용 RGB 데이터 조회 API 개발

--- src/components/bottomContent/searchResultBox.jsx
+++ src/components/bottomContent/searchResultBox.jsx
@@ -1,5 +1,20 @@
-function SearchResultBox() {
-  return <div className="bg-gray-500 p-8 m-10"></div>;
+function SearchResultBox(rgbData) {
+  return (
+    <div className="bg-gray-500 p-8 m-10">
+      {rgbData &&
+        Object.keys(rgbData).map((color, index) => (
+          <ul key={index}>
+            {Object.keys(color).map((col, key) => (
+              <span key={key}>
+                {Array.from(col).map((c, idx) => {
+                  return <div key={idx}>{"rgbArray: " + c[idx]}</div>;
+                })}
+              </span>
+            ))}
+          </ul>
+        ))}
+    </div>
+  );
 }
 
 export default SearchResultBox;
