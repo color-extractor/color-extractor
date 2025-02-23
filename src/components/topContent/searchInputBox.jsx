a40b561e0f31e2b460bf9216ed2cf233d2655da4
seonghoon <shee5032@naver.com>
2025-02-23 22:18:13 +0900

    feat: 키보드 enter Key로 검색하게 수정

--- src/components/topContent/searchInputBox.jsx
+++ src/components/topContent/searchInputBox.jsx
@@ -4,10 +4,16 @@ function SearchInputBox({ onSearch }) {
   const [url, setUrl] = useState("");
 
   const handleSearch = async () => {
+    if (url.trim() === "") {
+      return;
+    }
     await onSearch(url);
   };
 
   const handleInputUrl = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
     setUrl(event.currentTarget.value);
   };
 
@@ -15,11 +21,15 @@ function SearchInputBox({ onSearch }) {
     <div className="flex items-center gap-2 bg-gray-300">
       <input
         type="text"
-        className="flex-1 bg-gray-300 text-black outline-none"
+        className="flex-1 bg-gray-300 ml-2 text-black outline-none"
         value={url}
         onChange={(event) => {
           handleInputUrl(event);
         }}
+        onKeyDown={(event) => {
+          handleInputUrl(event);
+        }}
+        placeholder="url을 입력해주세요"
       />
       <button
         className="bg-black p-2 text-white"
