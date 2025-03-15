dea8141381d3281a6a96a2086b9f8685e50f8212
seonghoon <shee5032@naver.com>
2025-03-16 00:05:32 +0900

    refactor: SERVER_URL 관련 수정 작업

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import SearchResultBox from "./components/bottomContent/searchResultBox";
 import SearchInputBox from "./components/topContent/searchInputBox";
+import { SERVER_URL } from "./config/constants";
 import getMainColor from "./utils/getMainColor";
 
 function App() {
@@ -16,7 +17,7 @@ function App() {
     setInputUrl(checkUrl(url));
 
     const encodedUrl = encodeURIComponent(checkUrl(url));
-    const response = await fetch(`/api/crawl/${encodedUrl}`);
+    const response = await fetch(`${SERVER_URL}/crawl/${encodedUrl}`);
     const jsonResponseData = await response.json();
     const mainColor = getMainColor(jsonResponseData["data"]) || [];
 
